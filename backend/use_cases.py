affa6c5d87bccacd7728296f26b0e997fdeb678e
laponte243 <leyner.1002@gmail.com>
2023-06-02 13:37:28 -0400

    relaseBlocked

--- backend/use_cases.py
+++ backend/use_cases.py
@@ -13,35 +13,29 @@ def withdraw(user,asset,amount,type,description):
     new_transaction = Transaction(user_id=user,asset_id=asset,amount=amount,type=type,description=description)
     return new_transaction.save()
 
-@transaction.atomic
-def transfer(user_origin,user_destination,amount,asset):
-    ##block user_origin
-    blockFounds(user_origin,asset,amount)
-    ##block deposit destiny
-    blockDeposit(user_destination,asset,amount)
-    ##clear user_origin block
-    clearBlockFounds(user_origin,asset,amount)
-    ##release deposit destiny
-    releaseBlockFounds(user_destination,asset,amount)
-
 def blockFounds(user,asset,amount):
     if checkAvailable(user,asset,amount):
         balance = UserBalance.objects.get(user=user,asset=asset)
         balance.available -= amount
         balance.blocked +=amount
 
-def blockDeposit(user,asset,amount):
-    balance = UserBalance.objects.get(user=user,asset=asset)
-    balance.blocked +=amount
+def ReleaseBlockedFounds(user_origin,user_destination,amount,asset):
+    balance_origin = UserBalance.objects.get(user=user_origin,asset=asset)
+    balance_destination = UserBalance.objects.get(user=user_destination,asset=asset)
+    balance_origin.blocked = balance_origin.blocked - ammount
+    balance_destination.available = balance_destination.available + ammount
 
-def clearBlockFounds(user,asset,amount):
-    balance = UserBalance.objects.get(user=user,asset=asset)
-    balance.blocked -=amount
 
-def releaseBlockFounds(user,asset,amount):
-    balance = UserBalance.objects.get(user=user,asset=asset)
-    balance.available += amount
-    balance.blocked -=amount
+##transfer method
+@transaction.atomic
+def transfer(user_origin,user_destination,amount,asset):
+    ##block user_origin
+    blockFounds(user_origin,asset,amount)
+    ##release blocked founds to destination
+    ReleaseBlockedFounds(user_origin,user_destination,amount,asset)
 
-def checkAvailable(user,asset,amount):
-    return True if UserBalance.objects.get(user=user,asset=asset).available >= amount else False
+
+
+
+
+    
